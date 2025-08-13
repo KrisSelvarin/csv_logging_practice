b1f3e1d1f8304ff528b71f08a151267cb54ff624
KrisSelvarin <cdreduction@gmail.com>
2025-08-13 15:24:54 +0800

    Refactored code to use classes
    I haven't added the Summary function

--- rcr_creit.py
+++ rcr_creit.py
@@ -1,204 +1,198 @@
-# this is still drip simulation
-# i'll be using this for PSEI: RCR and CREIT personally
-
+# class implementation
 import csv
 
-def main():
-    sel = menu()
-    constant = branch(sel)
-    user = user_input()
-    result = compute(user, constant)
-    invest = user | result                          # merges both user and result dictionaries
-    summary(invest, constant)
-    read(constant)
+class Name:
+    def __init__(self, company, ticker):
+        self.company = company.upper()
+        self.ticker = ticker.upper()
 
+class Stock:
+    def __init__(self, name, pps, div_yield, payout, board_lot):
+        self.name = name
+        self.pps = pps
+        self.div_yield = div_yield
+        self.payout = payout
+        self.board_lot = board_lot
 
-# start up menu
-def menu():
+    def get_info(self):
+        if self.payout == 12:
+            freq = 'Monthly'
+        if self.payout == 4:
+            freq = 'Quarterly'
+        if self.payout == 2:
+            freq = 'Bi-monthly'
+        if self.payout == 1:
+            freq = 'Annually'
 
-    print("\nDRIP SIMULATION FOR RCR AND CREIT\n")
-    print("Choose Stock: ")
-
-    c = {
-        1: "CITICORE ENERGY REIT CORP. (CREIT)",
-        2: "RL COMMERCIAL REIT INC. (RCR)",
-        3: "EXIT",
-
-    }
-
-    for i in c:
-        print(f"[{i}] {c[i]}")
-
-    while True:
-        try:
-            x = int(input("Selection: "))
-            if x in c:
-                return x
-            else:
-                print("Enter value between 1to 3!\n")
-        except ValueError:
-            print("Invalid Input!\n")
-
-def branch(sel):
-
-    if sel == 1:
-        return creit()
-    elif sel == 2:
-        return rcr()
-    else:
-        exit()
-
-
-def creit():
-    print("""
-          CITICORE ENERGY REIT CORP. (CREIT)
-
-          Price per Share:          ₱3.68
-          Yield % (Indicated):      5.49%
-          Dividend Pay-out:         Quarterly
-          Minimum Board Lot:        100 shares
-    """)
-    
-    # CREIT constant
-    name = "CITICORE ENERGY REIT CORP. (CREIT)"
-    pps = 3.68
-    div_yield = 5.49 / 100
-    board_lot = 1000
-
-    #return
-    return {'name': name, 'pps': pps, 'div_yield': div_yield, 'board_lot': board_lot}
-
-def rcr():
-    print("""
-          RL COMMERCIAL REIT INC. (RCR)
-
-          Price per Share:          ₱7.90
-          Yield % (Indicated):      5.22%
-          Dividend Pay-out:         Quarterly
-          Minimum Board Lot:        100 shares
-    """)
-
-    # RCR constant
-    name = "RL COMMERCIAL REIT INC. (RCR)"
-    pps = 7.90
-    div_yield = 5.22 / 100
-    board_lot = 100
-
-    #return
-    return {'name': name, 'pps': pps, 'div_yield': div_yield, 'board_lot': board_lot}
-
-
-def user_input():
+        print(
+            f'\n{self.name.company} ({self.name.ticker})\n\n'
+            f'Price per Share:      ₱{self.pps:.2f}\n'
+            f'Yield % (Indicated):  {self.div_yield}%\n'
+            f'Dividend Pay-out:     {freq}\n'
+            f'Minimum Board Lot:    {self.board_lot} shares'
+        )
         
-    def dep_input():    
-        while True:
-            try:
-                dep = round(float(input("Monthly Deposit: ₱")), 2)
-                if dep > 0:
-                    return dep
-                else:
-                    print("Enter value greater than 0!\n")
-            except ValueError:
-                print("Invalid Input!\n")
+class Market:
+    def __init__(self):
+        self.stock_market = []
+    
+    def add_stock(self, stock):
+        self.stock_market.append(stock)
 
-    def time_input():
-        while True:
-            try:
-                t = int(input("Investment Duration (Years): "))
-                if 0 < t <= 60:
-                    return t
-                else:
-                    print("Enter value between 0 to 60!\n")
-            except ValueError:
-                print("Invalid Input!\n")
+    def display_stocks(self):
+        if not self.stock_market:
+            print('No stocks were added!')
+        else:
+            for stock in self.stock_market:
+                stock.get_info()
+                print()
 
-    return {
-        "dep": dep_input(),
-        "time": time_input(),
+class User:
+    def __init__(self, dep, time):
+        self.dep = dep
+        self.time = time
 
-    }
+    def user_info(self):
+        print(
+            f'Monthly Investment:   ₱{self.dep:.2f}\n'
+            f'Investment Duration:  {self.time} Years\n'
+        )
 
+    @classmethod
+    def user_input(cls):
+        print()
+        dep = float(input('Monthly Investment: ₱'))
+        time = int(input('Investment Duration (Years): '))
+        return cls(dep, time)
+    
+class Calculation:
+    result = []
 
-def compute(user, constant):
+    @classmethod
+    def calculate(cls, selection, user):
+        cls.result = []         # reset before calculations
+        # what stock to use
+        if selection == 1:
+            stock = stock_1
+        else:
+            stock = stock_2
 
-    # constants
-    freq = 4
-    board_lot_price = constant['board_lot'] * constant['pps']
+        # constants
+        board_lot_price = stock.board_lot * stock.pps
+        dps = ((stock.div_yield / 100) * stock.pps) / stock.payout
+        bp_total = 0
+        t_shares = 0
 
-    bp_total = 0
-    t_shares = 0
+        # calculations
+        for t in range(user.time):
+            for f in range(stock.payout):
+                for _ in range(12 // stock.payout):
+                    bp_total += user.dep
+                    n_shares = (bp_total // board_lot_price) * stock.board_lot
+                    t_shares += n_shares
+                    bp_used = n_shares * stock.pps
+                    bp_total -= bp_used
 
-    # dividend per share per quarter
-    dps = (constant['div_yield'] * constant['pps']) / freq
-
-    # csv file (example: rcr_div.csv)
-    filename = constant['name'].split('(')[-1].strip(')').lower() + f"_div.csv"
-    header = ['Year', 'Quarter', 'Dividends', 'Shares']
-
-    with open(filename, 'w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=header)
-        writer.writeheader()
-
-        # DRIP
-        for t in range(user['time']):
-            for f in range(freq):
-                for _ in range( 12 // freq):
-                    bp_total += user['dep']                                             # add deposit to buying power
-                    n_shares = (bp_total // board_lot_price) * constant['board_lot']    # no. of shares (100 board lot min)
-                    t_shares += n_shares                                                # accumulate shares
-                    bp_used = n_shares * constant['pps']                                # amount used on shares
-                    bp_total -= bp_used                                                 # remaining amount
-                    
-                # add dividend to bp
                 dividend = dps * t_shares
                 bp_total += dividend
 
-                # Log
-                writer.writerow({'Year': t + 1, 
-                                 'Quarter': f + 1, 
-                                 'Dividends': f'{dividend:.2f}', 
-                                 'Shares': int(t_shares)
+                cls.result.append({
+                    'Year': t + 1,
+                    'Quarter': f + 1,
+                    'Dividends': f'{dividend:.2f}',
+                    'Shares': int(t_shares)
                 })
 
-    return {
-        'bp_total': bp_total,
-        'shares': t_shares,
+    @classmethod
+    def logging(cls, selection):
+        # what stock to use
+        if selection == 1:
+            stock = stock_1
+        else:
+            stock = stock_2
 
-    }
+        filename = stock.name.ticker.lower() + '_div.csv'
+        header = ['Year', 'Quarter', 'Dividends', 'Shares']
+
+        with open(filename, 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=header)
+            writer.writeheader()
+
+            for row in cls.result:
+                writer.writerow(row)
+
+# reassign
+market = Market()
+
+# add creit
+name_creit = Name('CITICORE ENERGY REIT CORP.', 'CREIT')
+stock_1 = Stock(name_creit, 3.68, 5.49, 4, 1000)
+market.add_stock(stock_1)
+
+# add rcr
+name_rcr = Name('RL COMMERCIAL REIT INC.', 'RCR')
+stock_2 = Stock(name_rcr, 7.90, 5.22, 4, 100)
+market.add_stock(stock_2)
 
 
-def summary(invest, constant):
-        
-        gained = (invest['shares'] * constant['pps']) + invest['bp_total']
-        total_deposit = invest['dep'] * 12 * invest['time']
-        roi = (gained - total_deposit) / total_deposit
+def menu():
 
-        print(f"""
---------------------------------------------------------              
-SUMMARY:
-{constant['name']}
+    while True:
+    
+        print("\nDRIP SIMULATION FOR RCR AND CREIT\n")
+        print("Choose Stock: ")
 
-Price per Share:            ₱{constant['pps']}
-Yield % (Indicated):        {constant['div_yield']:.2%}
-Monthly Investment:         {invest['dep']:,.2f}
-Investment Duration:        {invest['time']} years
-Total Investment:           ₱{total_deposit:,.2f}
+        c = {
+            1: f"{stock_1.name.company} ({stock_1.name.ticker})",
+            2: f"{stock_2.name.company} ({stock_2.name.ticker})",
+            3: "Display Stocks' Info",
+            4: "Exit",
 
-Total No. of Shares:        {invest['shares']:,.0f}
-Total Amount of Shares:     ₱{invest['shares'] * constant['pps']:,.2f}
+        }
 
-Return on Investment:       {roi:.2%}
-""")
+        for i in c:
+            print(f'[{i}] {c[i]}')
 
+        def selection():
+            while True:
+                try:
+                    x = int(input('Selection: '))
+                    if x in c:
+                        return x
+                    else:
+                        print('Enter value between 1 to 4!\n')
+                except ValueError:
+                    print('Invalid Selection!\n')
 
-def read(constant):
+        def branch(x):
+            if x == 1:
+                return x
+            elif x == 2:
+                return x
+            elif x == 3:
+                market.display_stocks()
+                back = input('Back to Menu (Y|N): ').strip().lower()
+                if back != 'y':
+                    exit()
+            else:
+                exit()
+
+        x = selection()
+        choice = branch(x)
+        if choice == 1 or choice == 2: 
+            return branch(x)
+
+def read(selection):
     print("--------------------------------------------------------")
-
-    choice = input("Open Dividend Log (Y/N)? ").lower()
+    choice = input("Open Dividend Log (Y|N)? ").strip().lower()
+    if selection == 1:
+        stock = stock_1
+    else:
+        stock = stock_2
 
     if choice == 'y':
-        print("\nDividend Log\n")
-        filename = constant['name'].split('(')[-1].strip(')').lower() + f"_div.csv"
+        print(f'\nDividend Log:\n')
+        filename = stock.name.ticker.lower() + '_div.csv'
         with open(filename, 'r') as file:
             read = csv.DictReader(file)
             prev_year = None
@@ -215,13 +209,13 @@ def read(constant):
                 print(f"Dividend in Year {year}, Quarter {quarter}: ₱{dividends:>10,.2f} | Shares: {shares:>7,.0f}")
                 prev_year = year
 
-    elif choice == 'n':
-        exit()
-    else:
-        print('Invalid Input! Exiting Program!')
-        exit()
+def main():
+    selection = menu()
+    user = User.user_input()
+    Calculation.calculate(selection, user)
+    Calculation.logging(selection)
+    read(selection)
 
-
-
-if __name__ == "__main__":
+    
+if __name__ == '__main__':
     main()
